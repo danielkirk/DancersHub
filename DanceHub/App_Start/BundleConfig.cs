00f8e106501b74abd341291ec55603a2a5a39624
imdanielkirk <imdanielkirk@gmail.com>
2019-02-26 01:01:35 -0800

    Created a View of a table for all Dancers in the database

--- DanceHub/App_Start/BundleConfig.cs
+++ DanceHub/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace DanceHub
 {
@@ -20,7 +19,7 @@ namespace DanceHub
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
---
+++ DanceHub/Controllers/DancersViewController.cs
@@ -0,0 +1,41 @@
+﻿using DanceHub.Models;
+using DanceHub.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DanceHub.Controllers
+{
+    public class DancersViewController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: DancersView
+        public async Task<ActionResult> Dancer()
+        {
+            var dancer = AutoMapper.Mapper.Map<IList<DancerDTO>>(await db.Dancers.ToListAsync());
+
+            return View(dancer);
+        }
+
+        public ActionResult Index(int? PageIndex, string SortBy)
+        {
+            if (!PageIndex.HasValue)
+            {
+                PageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(SortBy))
+                SortBy = "Name";
+
+            return Content(String.Format("Page Index = {0}, Sort By = {1}", PageIndex, SortBy));
+        }
+
+        [Route("DancersView/DanceExperience/{danceExperience}")]
+        public ActionResult ByDanceExperience(int danceExperience)
+        {
+            return Content(danceExperience.ToString());
+        }
+    }
+}
